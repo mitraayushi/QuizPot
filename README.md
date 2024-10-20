cc8d1e59977bd15b2674420975e33039ea29f7f7
manjeet0796 <manjeetsharma0796@gmail.com>
2024-10-20 20:26:40 +0530

    functional and aia transaction

--- README.md
+++ README.md
@@ -1,10 +1,17 @@
 # Setup the project using
 
+```sh
 yarn install
 npm install
+```
 
 Add this token as manually into your wallet QPT
 
-Contract Address = 0xf9E6eE09433D72484B7aED6bafa3F54Ca647dEfc
+Contract Address = 0xc4D12645D522D3dc1BDf67Bcb04f1B4a8EbDc918
+
+Verify this Contract -> [Verify QPT](https://testnet.aiascan.com/token/0x247134A1a93Ab541B27905CFf96ad9d942C2Ea9a)
 
 ---
+
+# About QuizPot (build on AIA Testnet chain)
+
--- hardhat.config.ts
+++ hardhat.config.ts
@@ -17,16 +17,27 @@ const config: HardhatUserConfig = {
     },
   },
   networks: {
-    opencampus: {
-      url: `https://open-campus-codex-sepolia.drpc.org/`,
+    // opencampus: {
+    //   url: `https://open-campus-codex-sepolia.drpc.org/`,
+    //   accounts: process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
+    // },
+    // scrollTestnet: {
+    //   url: process.env.SCROLL_TESTNET_URL || "",
+    //   chainId: 534351,
+    //   accounts:
+    //     process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
+    // },
+    neoTestnet: {
+      url: `https://testnet.rpc.banelabs.org`,
       accounts: process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
+      chainId: 12227332,
+      gasPrice: 60000000000,
     },
-    scrollTestnet: {
-      url: process.env.SCROLL_TESTNET_URL || "",
-      chainId: 534351,
-      accounts:
-        process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
-    },
+    aia: {
+      url: 'https://aia-dataseed1-testnet.aiachain.org',
+      accounts: process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
+      chainId: 1320,
+    }
   },
 };
 
